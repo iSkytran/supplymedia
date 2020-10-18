4d15b27aeeb3c520a69ce164f7295bec49ff3815
Senih Tosun <btsenih@gmail.com>
2020-10-18 11:43:31 -0400

    Push

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # Supplymedia
+Supplymedia was create to tackle the issue of finding medical and health supplies during 
+crisis and pandemic related times. In a social media type interface, users are able to upload
+the type of supply (such as a face mask) that they have found,the availability of the supply 
+and a picture proof of the supply. They can then post the supply on the feed which other users can 
+then take a look at. The other users are then able to find what store has the supply available and
+can then purchase it from the store. Supplymedia's feed displays the picture, the user's name who posted the photo, the availability, the city and the store to help users find the nearest available store that someone has posted that has the supply they need in it.
+
+ Creators:
+ Sanjit Verma 
+ github @sanjitkverma
+
+ Ethan Tran   
+ github @iSkytran
+
+ Zachary Wilson
+ github @ZTW21
+
+ Senih Tosun
+ github @senihtosun
+
 
 ## Downloading Source
 
@@ -23,3 +43,4 @@ flask run
 ```
 
 The website can be accessed at `localhost:5000`.
+
--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template, url_for, flash, jsonify
 import gridfs, random, uuid, os
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 from datetime import datetime
 
-app = Flask(__name__)
-            # static_url_path = '',
-            # static_folder = 'static',
-            # template_folder = 'templates')
+app = Flask(__name__,
+            static_url_path = '',
+            static_folder = 'static',
+            template_folder = 'templates')
 
+app.config['SECRET_KEY'] = 'big secrets'    
 photos = UploadSet('photos', IMAGES)
 app.config['UPLOAD_FOLDER'] = 'images_store'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # Database setup
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -19,46 +22,102 @@ db = SQLAlchemy(app)
 # SQL form items
 class PostItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # name = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     storeItem = db.Column(db.String(80), unique=False, nullable=False)
     avalability = db.Column(db.String(80), unique=False, nullable=False)
-    time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    location = db.Column(db.String(80), unique=False, nullable=False)
+    #time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'storeItem': self.storeItem,
+            'avalability': self.avalability,
+            'location': self.location,
+            #'time': self.time.strftime("%A, %b %d, %Y, %X")
+        }
 
     def __repr__(self):
-        return '<ID %r>' % self.id
+        return (id, name, storeItem, avalability, location)
 
 db.create_all()
-# post = PostItem(name="John", storeItem="Hand Sanatizer", avalability="Avaliable")
-# db.session.add(post)
-# db.session.commit()
-# print(PostItem.query.all())
+
+def get_posts():
+    post = PostItem(name = 'a', storeItem = 'b', avalability = 'c', location = 'd')
+    db.session.add(post)
+    db.session.commit()
+    query = [i.__dict__ for i in PostItem.query.all()]
+    for item in query:
+        del item['_sa_instance_state']
+    print(query)
+    return query
+
 
 # Render webpages
 @app.route("/")
 def render_index():
-    return render_template("index.html")
+    return render_template("index.html", posts = get_posts())
 
-@app.route("/upload", methods=['GET', 'POST'])
+@app.route('/about')
+def render_about():
+    return render_template('about.html')
+
+@app.route("/upload/", methods=['GET', 'POST'])
 def render_upload():
-
     # Get form data
     if request.method == 'POST':
 
         # Check if the form is empty
-        if 'photo' not in request.files:
-            return 'there is no photo in form'
-
+        item = ""
         if '--------' == request.form.get('storeItem'):
-            return 'no store item selected'
+            redirected = redirect(url_for('render_upload'))
+            flash('Please select store item.')
+            return redirected
+        elif 'Other' == request.form.get('storeItem'):
+           item = request.form.get('Other')
+        else:
+            item = request.form.get('storeItem')
 
-        if '' == request.form.get('radio'):
-            return 'none selected'
+        if None is request.form.get('radio'):
+            redirected = redirect(url_for('render_upload'))
+            flash('Please select an availability option.')
+            return redirected
 
+        if '' == request.form.get('Name'):
+            redirected = redirect(url_for('render_upload'))
+            flash('Please enter a name.')
+            return redirected
+
+        if '' == request.form.get('location'):
+            redirected = redirect(url_for('render_upload'))
+            flash('Please enter a location.')
+            return redirected
+
+        if '' == request.form.get('store'):
+            redirected = redirect(url_for('render_upload'))
+            flash('Please enter a store.')
+            return redirected
+
+        if 'photo' not in request.files:
+            redirected = redirect(url_for('render_upload'))
+            flash('Please upload a photo.')
+            return redirected
+
+        file = request.files['photo']
+        if '' == file.filename:
+            redirected = redirect(url_for('render_upload'))
+            flash('No photo selected')
+            return redirected
+
+        locationStr = request.form.get('location') + ': ' + request.form.get('store')
+        
         # Save to database
-        post = PostItem(storeItem = request.form.get('storeItem'), avalability = request.form.get('radio'))
+        post = PostItem(name = request.form.get('Name'), storeItem = item, avalability = request.form.get('radio'), location = locationStr)
         db.session.add(post)
         db.session.commit()
-
+    
         # Save the photo in the upload folder
         photo = request.files['photo']
         path = os.path.join(app.config['UPLOAD_FOLDER'], str(post.id))
@@ -69,4 +128,7 @@ def render_upload():
 
         return redirect(url_for('render_index'))
 
-    return render_template("upload.html")
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+  app.run('0.0.0.0', 3000)
